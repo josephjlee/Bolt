96f009099fe9384f452f393dac470e3fa663e147
VEERAJ SHENOY <veerajthegreat@gmail.com>
2017-05-09 09:56:52 +0530

    SQLITE option

--- definations.php
+++ definations.php
@@ -103,7 +103,8 @@ if (in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
     define("ROOT", "/");
 }
 
-define("USEPDO", "NO");
+define("USEPDOMYSQL", "NO");
+define("USEPDOSQLITE", "NO");
 
 //Enable Pagenot Found
 define("PAGENOTFOUNDREDIRECT", "YES"); //SET LINK NAME IN index.php on line no
